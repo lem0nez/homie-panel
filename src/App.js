25ae2a85701bac2816229e63bc847c17db03b165
Nikita Dudko <nikita.dudko.95@gmail.com>
2024-07-29 12:09:50 +0300

    feat: store authToken cookie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import './App.css';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const authToken = params.get('authToken');
+    if (authToken) {
+      const date = new Date();
+      date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000)); // 30 days
+      document.cookie = `authToken=${authToken}; expires=${date.toUTCString()}; path=/`;
+    }
+  }, [location]);
+
   return (
     <div className="App">
       <div className="AppBar">
