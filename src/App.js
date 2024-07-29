083285b799f7a8e38113c3dce84076a2ad8cf16f
Nikita Dudko <nikita.dudko.95@gmail.com>
2024-07-29 12:03:12 +0300

    feat: add AppBar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="AppBar">
+        <div className="AppBar-content">
+          <img src="/logo192.png" alt="logo" className="AppBar-logo" />
+          <h1 className="AppBar-title">Raspberry control</h1>
+        </div>
+      </div>
       <header className="App-header" />
     </div>
   );
