d862e1647aa75edb28ecf7d7d2b313a78b999df1
Nikita Dudko <nikita.dudko@coinweb.io>
2024-11-13 16:00:57 +0300

    chore: update style

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,19 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
     warnOnUnsupportedTypeScriptVersion: false
   },
-  plugins: ['react-refresh'],
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': 'warn',
-    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-refresh/only-export-components': "warn",
+    '@typescript-eslint/no-unused-vars': "warn",
   },
-}
+};
