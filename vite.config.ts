c801f3116269671e9fdd09f4bd1bf5004d4d7f59
Nikita Dudko <nikita.dudko.95@gmail.com>
2024-11-24 18:54:22 +0300

    feat: set up proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://" + env.VITE_API_HOST,
+        },
+        "/api/graphql": {
+          target: "ws://" + env.VITE_API_HOST,
+          ws: true,
+        }
+      }
+    }
+  };
 });
